c43fc93fcd0c7ca7de2cdf277cdd55d186c34650
hungtp-genki <hung-tp@genkisystem.com>
2022-03-08 17:30:01 +0700

    Add TodoService

---
+++ lib/services/todo.service.dart
@@ -0,0 +1,68 @@
+import 'package:todo_flutter_demo/entities/todo.entity.dart';
+import 'package:todo_flutter_demo/infrastructures/hive.db.dart';
+
+import '../features/add_todo/todo.factory.dart';
+
+class TodoService {
+  late HiveInfra _hiveInfra;
+  late TodoFactory _todoFactory;
+  List<TodoEntity> _todos = [];
+  TodoService() {
+    _hiveInfra = HiveInfra();
+    _todoFactory = TodoFactory();
+  }
+
+  TodoEntity newTodo() {
+    return _todoFactory.create();
+  }
+
+  Future<List<TodoEntity>> addHead(TodoEntity todo) async {
+    try {
+      await _hiveInfra.addTodo(todo);
+      _todos = [todo, ..._todos];
+      return _todos;
+    } catch (err) {
+      rethrow;
+    }
+  }
+
+  Future<List<TodoEntity>> updateTodo(TodoEntity todo) async {
+    try {
+      await _hiveInfra.updateTodo(todo);
+      final indexOfTodo = _todos.indexOf(todo);
+      _todos[indexOfTodo] = todo;
+      return _todos;
+    } catch (err) {
+      rethrow;
+    }
+  }
+
+  Future<List<TodoEntity>> allTodos() async {
+    try {
+      _todos = await _hiveInfra.getAllTodos();
+      return _todos;
+    } catch (err) {
+      rethrow;
+    }
+  }
+
+  Future<List<TodoEntity>> clearAllTodos() async {
+    try {
+      await _hiveInfra.clearAllTodos();
+      _todos = [];
+      return _todos;
+    } catch (err) {
+      rethrow;
+    }
+  }
+
+  Future<List<TodoEntity>> removeTodo(TodoEntity todo) async {
+    try {
+      await _hiveInfra.removeTodo(todo);
+      _todos = _todos.where((t) => t.id != todo.id).toList();
+      return _todos;
+    } catch (err) {
+      rethrow;
+    }
+  }
+}
