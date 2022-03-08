34695e78201cae84b5ae14a98ac615d7a75dc05b
hungtp-genki <hung-tp@genkisystem.com>
2022-03-08 15:55:26 +0700

    Add todo entity

--- lib/entities/todo.entity.dart
+++ lib/entities/todo.entity.dart
@@ -1,14 +1,42 @@
-class Entity {
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class TodoEntity extends HiveObject {
+  @HiveField(0)
   late String id;
+
+  @HiveField(1)
   late String name;
+
+  @HiveField(2)
   late bool done;
-  Entity(this.id, this.name, this.done);
+
+  TodoEntity(this.id);
+
   Map<String, dynamic> toJson() {
     return {"id": id, "name": name, "done": done};
   }
 
-  static Entity fromJson(Map<String, dynamic> json) {
-    return Entity(
-        json["id"] as String, json["name"] as String, json["done"] as bool);
+  static TodoEntity fromJson(Map<String, dynamic> json) {
+    return TodoEntity(json["id"] as String)
+        .withName(json["name"] as String)
+        .withDone(json["done"] as bool);
   }
+
+  TodoEntity withName(String name) {
+    name = name;
+    return this;
+  }
+
+  TodoEntity withDone(bool done) {
+    done = done;
+    return this;
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is TodoEntity && id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
